3ad6948a2b706ed055ada1a515bcb6c311b22611
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-24 12:50:57 +0530

    Added: Create LinkedList By Adding 30 And 56 To 70

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+
+public class LinkedList {
+	Node head;
+	Node tail;
+
+	public void push(int data) {
+		Node newNode = new Node(data);
+		if(head==null) {
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+			Node temp = head;
+			this.head = newNode;
+			newNode.next = temp;
+		}
+	}
+	public void print() {
+		if (head == null)
+		{
+			System.out.println("Linked list is empty");
+
+		}
+		else
+		{
+			Node temp = head;
+			while(temp != null)
+			{
+				System.out.print(temp.data+ " ");
+				temp = temp.next;
+			}
+		}
+	}
+}
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -2,20 +2,8 @@ package com.bridgelabz;
 
 public class LinkedListMain {
 	public static void main(String[] args) {
-		System.out.println("Created A Simple LinkedList: ");
-		Node firstNode = new Node (56);
-		Node secondNode = new Node (30);
-		Node thirdNode = new Node (70);
+		System.out.println("Add the data at the start:");
+		Operations.addDataAtStart();
 
-		Node Head= firstNode;
-		firstNode.next=secondNode;
-		secondNode.next=thirdNode;
-
-		Node temp =Head;
-		while(temp!=null)
-		{
-			System.out.println(temp.data);
-			temp=temp.next;
-		}
 	}
 }
