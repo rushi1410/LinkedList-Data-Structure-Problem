36c16b9f7529a111cd40ac00613ef3e8c949828e
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-24 12:04:17 +0530

    Added: Create A Simple LinkedList

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -2,6 +2,20 @@ package com.bridgelabz;
 
 public class LinkedListMain {
 	public static void main(String[] args) {
-		System.out.println("Welcome To LinkedList Data Structure Problem");
+		System.out.println("Created A Simple LinkedList: ");
+		Node firstNode = new Node (56);
+		Node secondNode = new Node (30);
+		Node thirdNode = new Node (70);
+
+		Node Head= firstNode;
+		firstNode.next=secondNode;
+		secondNode.next=thirdNode;
+
+		Node temp =Head;
+		while(temp!=null)
+		{
+			System.out.println(temp.data);
+			temp=temp.next;
+		}
 	}
 }
